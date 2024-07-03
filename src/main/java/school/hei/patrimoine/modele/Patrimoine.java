7fd942d5d3507f93004cd05b959d94c2f9b63155
RASOANIRINA Mialisoa Lisa <hei.lisa.30@gmail.com>
2024-07-03 11:51:27 +0300

    feat(Patrimoine): add debt management

--- src/main/java/school/hei/patrimoine/modele/Patrimoine.java
+++ src/main/java/school/hei/patrimoine/modele/Patrimoine.java
@@ -9,19 +9,27 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 
 public record Patrimoine(
-    String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
-    implements Serializable/*note(no-serializable)*/ {
+        String nom, Personne possesseur, LocalDate t, Set<Possession> possessions)
+        implements Serializable {
 
   public int getValeurComptable() {
     return possessions.stream().mapToInt(Possession::getValeurComptable).sum();
   }
 
+  public int getValeurComptableEnDevise(String codeDevise, double tauxChange, double tauxAppreciationAnnuel) {
+    int total = 0;
+    for (Possession possession : possessions) {
+      total += possession.getValeurComptableEnDevise(codeDevise, tauxChange, tauxAppreciationAnnuel);
+    }
+    return total;
+  }
+
   public Patrimoine projectionFuture(LocalDate tFutur) {
     return new Patrimoine(
-        nom,
-        possesseur,
-        tFutur,
-        possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
+            nom,
+            possesseur,
+            tFutur,
+            possessions.stream().map(p -> p.projectionFuture(tFutur)).collect(toSet()));
   }
 
   public Possession possessionParNom(String nom) {
